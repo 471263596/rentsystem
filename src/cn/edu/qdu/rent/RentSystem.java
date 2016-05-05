78a75d0217f87a9620eeaecf5b158134372dbef3
admin <admin@mypc>
2016-05-05 08:44:42 +0800

    rentSystem1.5

--- src/cn/edu/qdu/rent/RentSystem.java
+++ src/cn/edu/qdu/rent/RentSystem.java
@@ -14,16 +14,18 @@ public class RentSystem {
 		Scanner input=new Scanner(System.in);
 		System.out.println("要租几辆车：");
 		int n=input.nextInt();
-		//选择车辆类别
+		String[] cusType=new String[n];
+		int[] cusPrice=new int[n];
+		int[] cusDay=new int[n];//租赁天数
 		
 		
-		int days;//租赁天数
 		String[] carType={"别克商务","宝马550i","别克林荫大道"};
-		String[] busType={"<=16",">16"};
+		String[] busType={"<=16座",">16座"};
 		double total=0;
 		int i=0;
+		//选择车辆类别
 		do{
-			System.out.print("请输入车辆类别（1.轿车 2.客车）：");
+			System.out.print(i+1+".请输入车辆类别（1.轿车 2.客车）：");
 			int style=input.nextInt();
 			if(style==1){//轿车
 				boolean flag1=false;
@@ -34,9 +36,11 @@ public class RentSystem {
 					Car car=(Car)moto;
 					car.setType(carType[a-1]);
 					System.out.print("请输入租赁天数：");
-					days=input.nextInt();//获取天数
-					System.out.println(car.getType()+car.getVehicle_no()+" 日租费（元/天）："+car.getPrice()); 
-					total+=moto.rentType(days);
+					
+					cusDay[i]=input.nextInt();//获取天数
+					total+=moto.rentType(cusDay[i]);
+					cusType[i]=carType[a-1];//
+					cusPrice[i]=car.getPrice();//
 					
 				}while(flag1);
 				i++;
@@ -44,14 +48,16 @@ public class RentSystem {
 			else if(style==2){//客车
 				boolean flag1=false;
 				do{
-					System.out.print("请输入车型（1.<=16座  2.>16座）：");
+					System.out.print(".请输入车型（1.<=16座  2.>16座）：");
 					int a=input.nextInt();    
 					Bus bus=new Bus("鲁A4321");
 					bus.setSeatCount(busType[a-1]);
 					System.out.print("请输入租赁天数：");
-					days=input.nextInt();//获取天数
-					System.out.println(bus.getSeatCount()+bus.getVehicle_no()+" 日租费（元/天）："+bus.getPrice());
-					total+=bus.rentType(days);
+					cusDay[i]=input.nextInt();//获取天数
+					total+=bus.rentType(cusDay[i]);
+					cusType[i]=busType[a-1];//
+					cusPrice[i]=bus.getPrice();//
+					
 				}while(flag1);
 				i++;
 			}
@@ -62,6 +68,9 @@ public class RentSystem {
 			System.out.println("");
 			
 		}while(i<n);
+		for (int j = 0; j < n; j++) {
+			System.out.println("第"+(j+1)+"辆车车型："+cusType[j]+"\t租赁天数："+cusDay[j]+"\t车的日租费用："+cusPrice[j]+"\t此车租赁总费用："+(cusDay[j]*cusPrice[j]));
+		}
 		System.out.println("总计："+total);
 	}
     public static void main(String[] args) {
